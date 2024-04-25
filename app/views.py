1c9781148cd4791c7b80c9aa1b4401e3633028ff
Manuel Escalera <manuel.jh.escalera@gmail.com>
2024-04-25 17:20:54 -0400

    agregado texto a voz y voz a texto

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+import os
+import shutil 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -7,6 +9,15 @@ from django.contrib import messages
 from django.utils import timezone
 from .models import ArchivosUsuario, HistorialServicios, Servicio
 from django.http import HttpResponse
+from openai import OpenAI
+from pathlib import Path
+from google.cloud import storage
+from dotenv import load_dotenv
+import tempfile
+
+load_dotenv()
+
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
 @login_required(login_url='inicio-sesion')
 def editarCuenta(request):
@@ -101,7 +112,7 @@ def carga_archivo(request):
         servicio = get_object_or_404(Servicio, pk=servicio_id)
         
         archivo_usuario = ArchivosUsuario.objects.create(
-            url_archivo='prueba',
+            url_archivo='',
             nombre_archivo=nombre_archivo,
             tipo_servicio=servicio,
             id_usuario=request.user,
@@ -114,7 +125,79 @@ def carga_archivo(request):
             id_usuario=request.user,
             archivo_usuario=archivo_usuario
         )
-        
+
+        supported_formats = ['mp3', 'txt']
+        file_extension = archivo.name.split('.')[-1]
+
+        if file_extension not in supported_formats:
+            messages.error(request, "Formato de archivo no soportado.")
+            return redirect('carga-archivo')
+
+        if servicio_id == '1':  #Voz a texto
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.' + archivo.name.split('.')[-1]) as tmp:
+                for chunk in archivo.chunks():
+                    tmp.write(chunk)
+                tmp_path = Path(tmp.name)
+
+            client = OpenAI()
+            try:
+                with open(tmp_path, 'rb') as audio_file:
+                    transcription = client.audio.transcriptions.create(
+                        model="whisper-1",
+                        file=audio_file
+                    )
+                text_content = transcription.text
+
+                text_file_path = Path('media') / f'{nombre_archivo}.txt'
+                with open(text_file_path, 'w') as text_file:
+                    text_file.write(text_content)
+                
+                bucket_name = 'eduayuda_bucket'
+                public_url = upload_to_gcs(bucket_name, str(text_file_path), f'{nombre_archivo}.txt')
+                if public_url:
+                    make_blob_public(bucket_name, f'{nombre_archivo}.txt')
+                    archivo_usuario.url_archivo = public_url
+                    archivo_usuario.save()
+
+            finally:
+                if tmp_path.exists():
+                    tmp_path.unlink()
+                if text_file_path.exists():
+                    text_file_path.unlink()
+
+        if servicio_id == '2': #Texto a voz
+            file_content = archivo.read().decode('utf-8')
+            client = OpenAI()
+            response = client.audio.speech.create(
+              model="tts-1",
+              voice="alloy",
+              input=file_content
+            )
+            
+            media_path = Path('media')
+            media_path.mkdir(parents=True, exist_ok=True)
+
+            speech_file_path = media_path / f'{nombre_archivo}.mp3'
+            response.stream_to_file(str(speech_file_path))
+
+            bucket_name = 'eduayuda_bucket'
+            file_name = f'{nombre_archivo}.mp3'
+            public_url = upload_to_gcs(bucket_name, str(speech_file_path), f'{nombre_archivo}.mp3')
+            if public_url:
+                print(f"URL obtenida de GCS: {public_url}")
+                make_blob_public(bucket_name, file_name)
+                archivo_usuario.url_archivo = public_url
+                archivo_usuario.save()
+                print(f"URL almacenada en la base de datos: {archivo_usuario.url_archivo}")
+                try:
+                    os.remove(str(speech_file_path))
+                except OSError as e:
+                    print(f"Error al eliminar el archivo {speech_file_path}: {e}")
+            else:
+                print("No se pudo obtener la URL de GCS.")
+            archivo_usuario.url_archivo = public_url
+            archivo_usuario.save()
+
         return redirect('menu-usuario')
     else:
         return render(request, 'app/inicio.html')
@@ -128,3 +211,27 @@ def eliminar_archivo(request, archivo_id):
         return redirect('menu-usuario')
     else:
         return HttpResponse("Método no permitido", status=405)
+
+""" Utils """
+
+def upload_to_gcs(bucket_name, source_file_name, destination_blob_name):
+    try:
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(source_file_name)
+        public_url = f"https://storage.googleapis.com/{bucket_name}/{destination_blob_name}"
+        print(f"Archivo subido exitosamente: {public_url}")
+        return public_url
+    except Exception as e:
+        print(f"Error al subir archivo a Google Cloud Storage: {str(e)}")
+        return None
+    
+def make_blob_public(bucket_name, blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    blob.make_public()
+
+    print(f"Blob {blob_name} is now publicly accessible at {blob.public_url}")
